428f18aba27508fa0174de64ab4381272c184ad5
aldoud1799 <abdallah.abbas.jr@gmail.com>
2023-10-30 18:29:59 +0400

    final touch-1

--- server/public/app.js
+++ server/public/app.js
@@ -1,4 +1,4 @@
-const socket = io('ws://localhost:3500')
+const socket = io('https://rt-chat-app.onrender.com')
 
 const msgInput = document.querySelector('#message')
 const nameInput = document.querySelector('#name')
